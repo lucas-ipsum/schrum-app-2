1744ac29b606494385e2fa7b27e54d4c6a1af08c
lucas-ipsum <s0572357@htw-berlin.de>
2025-05-29 12:36:21 +0200

    added nuxt strapi module

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -6,4 +6,16 @@ export default defineNuxtConfig({
   devtools: { enabled: true },
   css: ["~/assets/css/main.css"],
   vite: { plugins: [tailwindcss()] },
+
+  modules: ["@nuxtjs/strapi"],
+
+  strapi: {
+    url: process.env.STRAPI_URL || "http://localhost:1337",
+    prefix: "/api",
+    admin: "/admin",
+    version: "v5",
+    cookie: {},
+    cookieName: "strapi_jwt",
+  },
+
 });
