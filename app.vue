cdacb197eaf63a6671ecd35fca4eb715903932be
lucas-ipsum <s0572357@htw-berlin.de>
2025-06-13 11:18:33 +0200

    added festival info detail request

--- app.vue
+++ app.vue
@@ -3,3 +3,19 @@
     <NuxtPage />
   </NuxtLayout>
 </template>
+
+<script setup>
+  const { find } = useStrapi();
+
+  const festivalInformation = ref(null);
+
+  // ### Events ###
+  onMounted(() => {
+    getData();
+  });
+
+  // ### API Requests ###
+  const getData = async () => {
+    festivalInformation.value = await find("event-detail");
+  };
+</script>
