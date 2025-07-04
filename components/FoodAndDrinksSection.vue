32b2d80ac9cf717cb00836ff6c2252ae5b94e2b6
lucas-ipsum <s0572357@htw-berlin.de>
2025-07-05 00:55:57 +0200

    added api data for food and drinks info page

--- components/FoodAndDrinksSection.vue
+++ components/FoodAndDrinksSection.vue
@@ -1,48 +1,42 @@
 <template>
-    <!-- Drinks -->
-    <div class="flex flex-col">
-        <span class="font-semibold text-xl w-fit px-1 bg-yellow-800">
-            Getränke
-        </span>
-        <b>Placeholder</b>
-        <ul class="text-lg mw-[400px]">
-            <li>
-                Placeholder
-            </li>
-        </ul>
-    </div>
-    <!-- Food -->
-    <div class="flex flex-col mt-4">
-        <span class="font-semibold text-xl w-fit px-1 bg-yellow-800">
-            Essen
-        </span>
-        <b class="mb-2">
-            Placeholder
-        </b>
-        <b>Grillen:</b>
-        <ul class="text-lg mw-[400px]">
-            <li>
-                Placeholder
-            </li>
-        </ul>
-        <b>Frühstück</b>
-        <ul class="text-lg mw-[400px]">
-            <li>
-               Placeholder
-           </li>
-
-        </ul>
-    </div>
+  <!-- Drinks -->
+  <div class="flex flex-col">
+    <span class="font-semibold text-xl w-fit px-1 bg-yellow-800">
+      Getränke
+    </span>
+    <div
+      class="text-lg mw-[400px] markdown-list"
+      v-html="contentDrinksList"
+    ></div>
+  </div>
+  <!-- Food -->
+  <div class="flex flex-col mt-4">
+    <span class="font-semibold text-xl w-fit px-1 bg-yellow-800"> Essen </span>
+    <div
+      class="text-lg mw-[400px] markdown-list"
+      v-html="contentFoodList"
+    ></div>
+  </div>
 </template>
 
-<style scoped>
-ul {
-list-style-type: circle; /* Change bullet style */
-/* Other list styles */
-}
+<script setup>
+  // ## props ##
+  const props = defineProps({
+    foodAndDrinks: {
+      type: Object,
+      required: true,
+    },
+  });
 
-ul li {
-margin-left: 22px;
-margin-bottom: 8px; /* Example: Add margin between list items */
-}
-</style>
+  // ## refs ##
+  const contentDrinksList = ref(null);
+  const contentFoodList = ref(null);
+
+  // Markdown
+  const { $markdown } = useNuxtApp();
+
+  if (props.foodAndDrinks) {
+    contentDrinksList.value = $markdown.render(props.foodAndDrinks.drinksList);
+    contentFoodList.value = $markdown.render(props.foodAndDrinks.foodList);
+  }
+</script>
--- pages/foodAndDrinks.vue
+++ pages/foodAndDrinks.vue
@@ -1,16 +1,33 @@
 <template>
-    <!-- Layout Head -->
-    <div class="flex gap-4 justify-center text-center mb-4">
-        <div class="my-auto">        
-            <h1 class="text-stone-300 my-auto text-5xl sm:text-4xl font-semibold">Title</h1>
-            <h1 class="text-stone-300 my-auto text-2xl sm:text-xl font-semibold">Date</h1>
-        </div>
+  <!-- Layout Head -->
+  <div class="flex gap-4 justify-center text-center mb-4">
+    <div class="my-auto">
+      <h1 class="text-stone-300 my-auto text-5xl sm:text-4xl font-semibold">
+        Title
+      </h1>
+      <h1 class="text-stone-300 my-auto text-2xl sm:text-xl font-semibold">
+        Date
+      </h1>
     </div>
-    <!-- Page content -->
-    <div class="text-center mb-5">
-        <h2 class="text-stone-300 text-3xl sm:text-5xl font-semibold">Food & Drinks</h2>
-    </div>
-    <div class="px-4 sm:px-16">
-        <FoodAndDrinksSection />
-    </div>
+  </div>
+  <!-- Page content -->
+  <div class="text-center mb-5">
+    <h2 class="text-stone-300 text-3xl sm:text-5xl font-semibold">
+      Food & Drinks
+    </h2>
+  </div>
+  <div class="px-4 sm:px-16">
+    <FoodAndDrinksSection :food-and-drinks="foodAndDrinks" />
+  </div>
 </template>
+
+<script setup>
+  // ## refs ##
+  const foodAndDrinks = ref(null);
+  // Strapi
+  const { find } = useStrapi();
+
+  // ## Api call ##
+  const res = await find("food-and-drinks");
+  foodAndDrinks.value = res.data;
+</script>
