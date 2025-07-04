328515d8e676f9447964952e9ccefad5b5101907
lucas-ipsum <s0572357@htw-berlin.de>
2025-07-05 00:08:06 +0200

    added api content for train section info page

--- components/travelInfo/TrainSection.vue
+++ components/travelInfo/TrainSection.vue
@@ -1,38 +1,46 @@
 <template>
-    <div class="flex flex-col">
-       <span class="font-semibold text-xl w-fit px-1 bg-yellow-800">
-            Bahnhof & Shuttle
-                </span>
-                <ul class="text-lg mw-[400px]">
-                    <li>
-                        Placeholder
-                    </li>
-                </ul>
+  <div class="flex flex-col">
+    <span class="font-semibold text-xl w-fit px-1 bg-yellow-800">
+      Bahnhof & Shuttle
+    </span>
+    <div
+      class="text-lg mw-[400px] markdown-list"
+      v-html="contentTrainShuttleInfo"
+    ></div>
 
-                <span class="font-semibold text-xl w-fit px-1 bg-yellow-800">
-             Shuttle Fahrtzeiten
-                </span>
-                <ul class="text-lg mw-[400px]">
-                    <li>
-                        Placeholder
-                    </li>
-                </ul>
-            </div>
+    <span class="font-semibold text-xl w-fit px-1 bg-yellow-800">
+      Shuttle Fahrtzeiten
+    </span>
+    <div
+      class="text-lg mw-[400px] markdown-list"
+      v-html="contentShuttleTimes"
+    ></div>
+  </div>
 </template>
 
 <script setup>
-// <InfoSectionVue title="Bahnhof" :textList="['HI']" />
+  // ## Props ##
+  const props = defineProps({
+    travelInformation: {
+      type: Object,
+      required: true,
+    },
+  });
 
+  // ## Refs ##
+  const contentTrainShuttleInfo = ref(null);
+  const contentShuttleTimes = ref(null);
+
+  // Markdown
+  const { $markdown } = useNuxtApp();
+
+  if (props.travelInformation) {
+    contentTrainShuttleInfo.value = $markdown.render(
+      props.travelInformation.trainShuttleInfo
+    );
+
+    contentShuttleTimes.value = $markdown.render(
+      props.travelInformation.shuttleTimes
+    );
+  }
 </script>
-
-<style scoped>
-ul {
-  list-style-type: circle; /* Change bullet style */
-  /* Other list styles */
-}
-
-ul li {
-margin-left: 22px;
-  margin-bottom: 8px; /* Example: Add margin between list items */
-}
-</style>
