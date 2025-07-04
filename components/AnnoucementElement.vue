f207c6a66beb213b04c19b89a9158db9abb3f14a
lucas-ipsum <s0572357@htw-berlin.de>
2025-07-05 01:38:20 +0200

    added api data for aannouncements

--- components/AnnoucementElement.vue
+++ components/AnnoucementElement.vue
@@ -1,23 +1,22 @@
 <template>
-    <div class="flex flex-col outline outline-2 py-1 px-2 outline-green-800">
-        <span class="font-semibold w-fit px-1 bg-yellow-800">
-            {{ annoucementDetails.title }}
-        </span>
-        <span>
-            {{ annoucementDetails.date }}
-        </span>
-        <span>
-            {{ annoucementDetails.content }}
-        </span>
-    </div>
+  <div class="flex flex-col outline-2 py-1 px-2 outline-green-800">
+    <span class="font-semibold w-fit px-1 bg-yellow-800">
+      {{ announcementDetails.category }}
+    </span>
+    <span>
+      {{ announcementDetails.date }}
+    </span>
+    <span>
+      {{ announcementDetails.content }}
+    </span>
+  </div>
 </template>
 
 <script setup>
-const props = defineProps({
-    annoucementDetails: {
-        type: Object,
-        required: true
-    }
-})
-
+  const props = defineProps({
+    announcementDetails: {
+      type: Object,
+      required: true,
+    },
+  });
 </script>
