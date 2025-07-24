2e7e4e70472a4fbb065f95d535e105404a7e2043
lucas-ipsum <s0572357@htw-berlin.de>
2025-07-24 14:10:45 +0200

    added favicon config

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -39,4 +39,25 @@ export default defineNuxtConfig({
     },
     cookieName: "strapi_jwt",
   },
+  app: {
+    head: {
+      link: [
+        {
+          rel: "icon",
+          type: "image/png",
+          sizes: "96x96",
+          href: "/favicon-96x96.png",
+        },
+        { rel: "icon", type: "image/svg+xml", href: "/favicon.svg" },
+        { rel: "shortcut icon", href: "/favicon.ico" },
+        {
+          rel: "apple-touch-icon",
+          sizes: "180x180",
+          href: "/apple-touch-icon.png",
+        },
+        { rel: "manifest", href: "/site.webmanifest" },
+      ],
+      meta: [{ name: "apple-mobile-web-app-title", content: "Schrum" }],
+    },
+  },
 });
