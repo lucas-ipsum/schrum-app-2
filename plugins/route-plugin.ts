d0280f39eefd7ce2024271185d4b0f2d9e57b5e5
lucas-ipsum <s0572357@htw-berlin.de>
2025-07-24 22:08:26 +0200

    fixed redirect to login page not working

--- plugins/route-plugin.ts
+++ plugins/route-plugin.ts
@@ -1,12 +1,15 @@
 export default defineNuxtPlugin(() => {
-const jwt = useCookie('strapi_jwt'); // or whatever name you used
+  const jwt = useCookie("strapi_jwt"); // or whatever name you used
   addRouteMiddleware(
     "global-middleware",
     (to, from) => {
-      console.log(useUserStore().isLoggedIn);
-      if (!useUserStore().isLoggedIn && to.path !== "/login" && !jwt) {
+      if (!useUserStore().isLoggedIn || !jwt) {
         // Redirect to the login page
-        return navigateTo("/login");
+        if (to.path == "/login") {
+          return;
+        } else {
+          return navigateTo("/login");
+        }
       }
     },
     { global: true }
