c548f78b8dab65cfcd593765797406251497873f
lucas-ipsum <s0572357@htw-berlin.de>
2025-08-05 14:41:56 +0200

    fixed request fails on double click on button

--- components/shared/SubmitButton.vue
+++ components/shared/SubmitButton.vue
@@ -1,6 +1,6 @@
 <template>
   <div>
-    <button @click="$emit('submit')" class="outline-2 p-2 flex font-semibold w-fit cursor-pointer">
+    <button @click="$emit('submit')" :disabled="disabled" class="outline-2 p-2 flex font-semibold w-fit" :class="disabled ? 'outline-gray-600 text-gray-600 cursor-not-allowed' : 'cursor-pointer'">
       <span> {{ text }} </span>
     </button>
   </div>
@@ -11,6 +11,10 @@ const props = defineProps({
     text: {
         type: String, 
         required: true
+    },
+    disabled: {
+      type: Boolean,
+      required: false
     }
 })
 
